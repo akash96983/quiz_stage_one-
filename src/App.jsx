5178f79901f31cdb1a3750042f420fffc5174e2d
Akash9698 <senthilsenthil98832@gmail.com>
2024-04-10 15:24:50 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Quiz from './components/Quiz'
+import Result from './components/Result'
+import Home from './components/Home'
+
+function App() {
+
+  return (
+    <>
+      <Home/>
+    </>
+  )
+}
+
+export default App
